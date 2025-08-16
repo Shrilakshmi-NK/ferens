774f01c4b5702a8e92e330e8d53f631c5fade957
Shrilakshmi-NK <ininidinji@gmail.com>
2025-08-16 14:12:40 +0530

    server folder changes

--- server/historyModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const historySchema = new mongoose.Schema({
-  original: String,
-  user: String
-}, { timestamps: true }); // Automatically adds createdAt and updatedAt
-
-module.exports = mongoose.model('History', historySchema);
